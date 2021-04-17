028caeb1c7526d9a54ade25acc16b9fca9542afa
Briam Martinez <briammartinez@gmail.com>
2021-04-17 21:23:46 +0200

    :sparkles: process-openapi endpoint implementation

--- api/index.js
+++ api/index.js
@@ -8,9 +8,27 @@ const config = require('../config.js');
 
 const app = express();
 
-expressJSDocSwagger(app)(config.api.swaggerOptions);
+let transformMethods = null;
+const instance = expressJSDocSwagger(app)(config.api.swaggerOptions);
+instance.on('finish', (_, transforms) => {
+  transformMethods = transforms;
+})
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan('tiny', { skip: () => process.env.NODE_ENV === 'test' }));
+app.post('/api/v1/process-openapi', (req, res) => {
+  const { payload } = req.body;
+  let swaggerObject = {
+    openapi: '3.0.0',
+    info: config.api.swaggerOptions.info,
+  };
+  const parsedJSDocs = transformMethods.jsdocInfo()(payload);
+  swaggerObject = transformMethods.getPaths(swaggerObject, parsedJSDocs);
+  swaggerObject = transformMethods.getComponents(swaggerObject, parsedJSDocs);
+  swaggerObject = transformMethods.getTags(swaggerObject, parsedJSDocs);
+  res.json(swaggerObject);
+})
 
 module.exports = app;
